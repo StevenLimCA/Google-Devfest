c263b0477c7546d69a189e62fbe4bc85350978ed
Steven Lim <steven-lim-ca>
2022-10-21 17:56:07 -0700

    refactored code to remove warning

--- src/components/sessions.js
+++ src/components/sessions.js
@@ -5,13 +5,14 @@ import { v4 as uuidv4 } from "uuid";
 export default function SessionSection() {
   const sessionStartTimes = ["10:30", "11:15", "12:00"];
   const sessionEndTimes = ["11:15", "12:00", "12:45"];
-  const AmOrFm = (el) => (el >= 12 ? "PM" : "AM");
+  const AmOrFm = (el) => (el.substring(0, 2) >= 12 ? "PM" : "AM");
   const [firstSessionsInfo, setFirstSessionInfo] = useState();
   const [secondSessionsInfo, setSecondSessionInfo] = useState();
   const [thirdSessionsInfo, setThirdSessionInfo] = useState();
 
   useEffect(() => {
     const { REACT_APP_SESSIONIZE_ID } = process.env;
+    const sessionStartTimes = ["10:30", "11:15", "12:00"];
     const fetchData = () => {
       axios(
         `https://sessionize.com/api/v2/${REACT_APP_SESSIONIZE_ID}/view/Sessions`
@@ -51,8 +52,8 @@ export default function SessionSection() {
       <div className=" bg-slate-100 rounded-2xl p-5">
         <h3>
           {" "}
-          {sessionStartTimes[0]} {AmOrFm(sessionStartTimes[0].substring(0, 2))}{" "}
-          - {sessionEndTimes[0]} {AmOrFm(sessionEndTimes[0].substring(0, 2))}
+          {sessionStartTimes[0]} {AmOrFm(sessionStartTimes[0])} -{" "}
+          {sessionEndTimes[0]} {AmOrFm(sessionEndTimes[0])}
         </h3>
         <div className="flex flex-wrap flex-col w-full px-10">
           {firstSessionsInfo
@@ -65,8 +66,8 @@ export default function SessionSection() {
       <div className="rounded-2xl p-5">
         <h3>
           {" "}
-          {sessionStartTimes[1]} {AmOrFm(sessionStartTimes[1].substring(0, 2))}{" "}
-          - {sessionEndTimes[1]} {AmOrFm(sessionEndTimes[1].substring(0, 2))}
+          {sessionStartTimes[1]} {AmOrFm(sessionStartTimes[1])} -{" "}
+          {sessionEndTimes[1]} {AmOrFm(sessionEndTimes[1])}
         </h3>
         <div className="flex flex-wrap flex-col w-full px-10">
           {secondSessionsInfo
@@ -79,8 +80,8 @@ export default function SessionSection() {
       <div className=" bg-slate-100 rounded-2xl p-5">
         <h3>
           {" "}
-          {sessionStartTimes[2]} {AmOrFm(sessionStartTimes[2].substring(0, 2))}{" "}
-          - {sessionEndTimes[2]} {AmOrFm(sessionEndTimes[2].substring(0, 2))}
+          {sessionStartTimes[2]} {AmOrFm(sessionStartTimes[2])} -{" "}
+          {sessionEndTimes[2]} {AmOrFm(sessionEndTimes[2])}
         </h3>
         <div className="flex flex-wrap flex-col w-full bg-slate-100 px-10">
           {thirdSessionsInfo
