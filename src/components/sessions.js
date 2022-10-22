7ef4241675bfce33bb0e7e3f9af9bd5619e78ea7
Steven Lim <steven-lim-ca>
2022-10-21 17:11:19 -0700

    fixed some issues with time

--- src/components/sessions.js
+++ src/components/sessions.js
@@ -3,7 +3,8 @@ import { useEffect, useState } from "react";
 import SessionBox from "./sessionbox";
 import { v4 as uuidv4 } from "uuid";
 export default function SessionSection() {
-  const sessionTimes = ["10:30", "11:15", "12:00"];
+  const sessionStartTimes = ["10:30", "11:15", "12:00"];
+  const sessionEndTimes = ["11:15", "12:00", "12:45"];
   const [firstSessionsInfo, setFirstSessionInfo] = useState();
   const [secondSessionsInfo, setSecondSessionInfo] = useState();
   const [thirdSessionsInfo, setThirdSessionInfo] = useState();
@@ -16,18 +17,18 @@ export default function SessionSection() {
       ).then((res) => {
         setFirstSessionInfo(
           res.data[0].sessions.filter(
-            (el) => el.startsAt.substring(11, 16) === sessionTimes[0]
+            (el) => el.startsAt.substring(11, 16) === sessionStartTimes[0]
           )
         );
 
         setSecondSessionInfo(
           res.data[0].sessions.filter(
-            (el) => el.startsAt.substring(11, 16) === sessionTimes[1]
+            (el) => el.startsAt.substring(11, 16) === sessionStartTimes[1]
           )
         );
         setThirdSessionInfo(
           res.data[0].sessions.filter(
-            (el) => el.startsAt.substring(11, 16) === sessionTimes[2]
+            (el) => el.startsAt.substring(11, 16) === sessionStartTimes[2]
           )
         );
       });
@@ -47,7 +48,10 @@ export default function SessionSection() {
         Date: {process.env.REACT_APP_DEVFEST_DATE}
       </h3>
       <div className=" bg-slate-100 rounded-2xl p-5">
-        <h3> {sessionTimes[0]} AM - 11:15 PM</h3>
+        <h3>
+          {" "}
+          {sessionStartTimes[0]} AM - {sessionEndTimes[0]} AM
+        </h3>
         <div className="flex flex-wrap flex-col w-full px-10">
           {firstSessionsInfo
             ? firstSessionsInfo.map((el) => (
@@ -57,7 +61,10 @@ export default function SessionSection() {
         </div>
       </div>
       <div className="rounded-2xl p-5">
-        <h3> {sessionTimes[1]} AM - 12:00 PM</h3>
+        <h3>
+          {" "}
+          {sessionStartTimes[1]} AM - {sessionEndTimes[1]} PM
+        </h3>
         <div className="flex flex-wrap flex-col w-full px-10">
           {secondSessionsInfo
             ? secondSessionsInfo.map((el) => (
@@ -67,7 +74,10 @@ export default function SessionSection() {
         </div>{" "}
       </div>
       <div className=" bg-slate-100 rounded-2xl p-5">
-        <h3> {sessionTimes[2]} AM - 12:45 PM</h3>
+        <h3>
+          {" "}
+          {sessionStartTimes[2]} PM - {sessionEndTimes[2]} PM
+        </h3>
         <div className="flex flex-wrap flex-col w-full bg-slate-100 px-10">
           {thirdSessionsInfo
             ? thirdSessionsInfo.map((el) => (
