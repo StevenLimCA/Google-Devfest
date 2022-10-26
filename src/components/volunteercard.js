8bae311daeeda83987d1d3a43a1738d4a902592a
Steven Lim <steven-lim-ca>
2022-10-25 18:28:06 -0700

    added volunteer social media icons

--- src/components/volunteercard.js
+++ src/components/volunteercard.js
@@ -3,9 +3,39 @@ import React from "react";
 export default function VolunteerCard({ volunteerInfo }) {
   return (
     <div>
-      {/* <img src={volunteerInfo.img} alt={volunteerInfo.name} className="" /> */}
+      {volunteerInfo.img ? (
+        <img
+          src={volunteerInfo.img}
+          alt={volunteerInfo.name}
+          className="rounded-full w-48 h-48"
+        />
+      ) : (
+        <div className="rounded-full w-48 h-48 bg-slate-300"></div>
+      )}
+
       <p>{volunteerInfo.name} </p>
+      <p className="italic">{volunteerInfo.title}</p>
       <p>{volunteerInfo.desc}</p>
+      <p className="flex justify-between">
+        <a href={volunteerInfo.linkedin} alt="linked-In">
+          <img src="./linkedin-in.svg" alt="linkedin" className="w-6 h-6" />
+        </a>
+        {volunteerInfo.website ? (
+          <a
+            href={volunteerInfo.website}
+            alt="their website"
+            className="flex flex-row"
+          >
+            <img
+              src="./link-solid.svg"
+              alt={`${volunteerInfo.name}'s website`}
+              className="w-6 h-6 mx-2"
+            />
+          </a>
+        ) : (
+          ""
+        )}
+      </p>
     </div>
   );
 }
--- src/pages/volunteer.js
+++ src/pages/volunteer.js
@@ -10,7 +10,16 @@ const Volunteer = () => (
       alt="By Hanna Busing"
     />
     <h1 className="text-center my-10">Meet Our Volunteers</h1>
-    <div className="grid grid-cols-1 lg:grid-cols-3 mx-5">
+    <p>
+      We would like to take this opportunity to thank our amazing team here at{" "}
+      {process.env.REACT_APP_CHAPTER_NAME}. Without a dedicated and highly
+      motivated team to manage the event, find sponsorships, develop the
+      website, take the initiatives, and coordinate the procurement of speakers,
+      food and the community, DevFest 2022 would not have been possible. So
+      please give them a huge round of applause! And remember to consider them
+      for your future tech projects!
+    </p>
+    <div className="grid grid-cols-1 lg:grid-cols-3 px-5">
       {volunteerInfo.map((el) => (
         <VolunteerCard volunteerInfo={el} />
       ))}
